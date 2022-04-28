7e741092146210fdb353f4db3723e23c83953c88
LeonardoBuscarioloGC <leonardo.buscariolo@grancoffee.com.br>
2022-04-27 23:07:17 -0300

    final version

--- script.js
+++ script.js
@@ -3,13 +3,50 @@ $("#whatsapp").click(function (e) {
     alert('Teste')
 });
 
+let windowWidth = $(window).innerWidth();
+
+if(windowWidth < 400) {
+    $('#navbarOne').removeClass('navbarDesktop')
+        $('#navbarOne').addClass('navbarDesktopNoShow')
+        $('#navbarTwo').removeClass('navbarMobileNoShow')
+        $('#navbarTwo').addClass('navbarMobile')
+        $('#quotationContainer').css({"display": "none"})
+        $('.uotationContainerClass').css({"display": "none"})
+
+        $('.imgCard').attr('src', "./imgs/banners/mobile-banner.png")
+        $('#collapse3').css({"width":"100vw", "padding-right": "20px"})
+} else {
+    $('#navbarOne').addClass('navbarDesktop')
+    $('#navbarOne').removeClass('navbarDesktopNoShow')
+    $('#navbarTwo').addClass('navbarMobileNoShow')
+    $('#navbarTwo').removeClass('navbarMobile')
+    $('#quotationContainer').css({"display": "flex"})
+    $('.uotationContainerClass').css({"display": "flex"})
+
+    $('.imgCard').attr('src', "./imgs/banners/desktop-.jpg")
+    $('#collapse3').css({"width":"20vw"})
+}
+
 $(window).scroll(function () { 
-    if(this.scrollY > 200) {
-        console.log('Andou')
-        $(".navbar").css({"display": "none"})
+    if(windowWidth > 400) {
+    if(this.scrollY >= 500) {
+        console.log(scrollY)
         $('#quotationContainer').css({"display": "none"})
     } else {
-        $(".navbar").css({"display": "flex"})
         $('#quotationContainer').css({"display": "flex"})
     }
+
+    if(this.scrollY >= 200) {
+        console.log(scrollY, 'DOISS')
+        $('#navbarOne').removeClass('navbarDesktop')
+        $('#navbarOne').addClass('navbarDesktopNoShow')
+        $('#navbarTwo').removeClass('navbarMobileNoShow')
+        $('#navbarTwo').addClass('navbarMobile')
+    } else {
+        $('#navbarOne').removeClass('navbarDesktopNoShow')
+        $('#navbarOne').addClass('navbarDesktop')
+        $('#navbarTwo').removeClass('navbarMobile')
+        $('#navbarTwo').addClass('navbarMobileNoShow')
+    } 
+}
 });
